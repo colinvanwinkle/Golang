654a7ba3c0bb9cf7085e66b412914829e30d5995
colinvw <cvanwinkle@miteksystems.com>
2017-06-30 10:28:39 -0700

    modified main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "github.com/PuerkitoBio/goquery"
   "strconv"
   "sync"
+  "strings"
 );
 
 func main(){
@@ -24,19 +25,43 @@ func main(){
   "ADI",
   "ADM",
   "ADP",
-  "ADSK" }
+  "ADSK",
+  "AEE",
+  "AEP",
+  "AES",
+  "AET",
+  "AFL",
+  "AGN",
+  "AIG",
+  "AIV",
+  "AIZ",
+  "AJG",
+  "AKAM",
+  "ALB",
+  "ALGN",
+  "ALK",
+  "ALL",
+  "ALLE",
+  "ALXN",
+  "AMAT",
+  "AMD",
+  "AME"}
 
   for _, ticker := range tickers{
-
     wg.Add(1);
     go func(ticker string){
       doc, _ := goquery.NewDocument("http://www.nasdaq.com/symbol/" + ticker + "/real-time");
-      priceChange, _ :=  strconv.ParseFloat(doc.Find("div#qwidget_netchange").Contents().Text(), 64);
+      priceChange, _ :=  strconv.ParseFloat(doc.Find("div#qwidget_percent").Contents().Text(), 64);
 
       if doc.Find(".marginLR10px.arrow-red").Length() == 1{
-        priceChange = -1 * priceChange;
+        wg.Done();
+        return;
       }
-        fmt.Println(priceChange);
+
+        tickersFormatTemp := []string{ticker, ":"};
+        ticker = strings.Join(tickersFormatTemp, "");
+        fmt.Println(ticker, priceChange);
+
         wg.Done();
       }(ticker)
 
