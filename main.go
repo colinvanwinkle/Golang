a80cdaf2b03219d3e7a2c48692dcb54e5823a04d
colinvw <cvanwinkle@miteksystems.com>
2017-06-29 17:10:32 -0700

    added goroutine for ticker lookup

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
   "fmt"
   "github.com/PuerkitoBio/goquery"
   "strconv"
+  "sync"
 );
 
 func main(){
+
+  var wg sync.WaitGroup;
+
   tickers := []string {
   "A",
   "AAL",
@@ -23,13 +27,19 @@ func main(){
   "ADSK" }
 
   for _, ticker := range tickers{
-    doc, _ := goquery.NewDocument("http://www.nasdaq.com/symbol/" + ticker + "/real-time");
-    priceChange, _ :=  strconv.ParseFloat(doc.Find("div#qwidget_netchange").Contents().Text(), 64);
 
-    if doc.Find(".marginLR10px.arrow-red").Length() == 1{
-      priceChange = -1 * priceChange;
-    }
-      fmt.Println(priceChange);
+    wg.Add(1);
+    go func(ticker string){
+      doc, _ := goquery.NewDocument("http://www.nasdaq.com/symbol/" + ticker + "/real-time");
+      priceChange, _ :=  strconv.ParseFloat(doc.Find("div#qwidget_netchange").Contents().Text(), 64);
+
+      if doc.Find(".marginLR10px.arrow-red").Length() == 1{
+        priceChange = -1 * priceChange;
+      }
+        fmt.Println(priceChange);
+        wg.Done();
+      }(ticker)
 
     }
+    wg.Wait();
 }
